54653bf979caa42a74b88b4033584646e2929781
Kimmy Chirnside <kimmy.chirnside@gmail.com>
2010-04-15 21:37:12 +1200

    Adding generation script to build new page form layout

--- rails/init.rb
+++ rails/init.rb
@@ -1,3 +1,19 @@
+require 'fileutils'
 gem_root = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+
 Cms.add_to_rails_paths gem_root
 Cms.add_generator_paths gem_root, "db/migrate/[0-9]*_*.rb"
+
+bcms_path = Gem.loaded_specs['browsercms'].full_gem_path
+page_view_dir = File.join('app', 'views', 'cms', 'pages')
+# copy core _form.html.erb to path in module
+FileUtils.mkdir_p(File.join(gem_root, page_view_dir))
+FileUtils.cp(File.join(bcms_path, page_view_dir, '_form.html.erb'), File.join(gem_root, page_view_dir, '_form.html.erb'))
+# now concat module specific code to end of new _form.html.erb
+File.open(File.join(gem_root, page_view_dir, '_form.html.erb'), 'a') do |f|
+  File.open(File.join(gem_root, 'lib', 'bcms_tagged_pages', 'generators', 'files', '_form.html.erb'), 'r') do |inner_f|
+    while line = inner_f.gets
+      f.puts(line)
+    end
+  end
+end
